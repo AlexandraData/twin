d194cd743040982a9be79e9008b26c9a54979440
Alexandra Machado <asg.machado@gmail.com>
2026-03-02 00:02:24 +0000

    Declare github_repository variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -51,3 +51,8 @@ variable "root_domain" {
   type        = string
   default     = ""
 }
+
+variable "github_repository" {
+  description = "The owner and name of the GitHub repository"
+  type        = string
+}
